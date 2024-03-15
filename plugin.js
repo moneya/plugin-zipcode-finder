2028f8f6a2ae2d4602b1df22238d8770d0849248
Moneya <moneya59@gmail.com>
2024-03-15 01:27:18 +0100

    clean the header

--- plugin.js
+++ plugin.js
@@ -12,7 +12,6 @@ function run() {
     url: apiUrl,
     headers: {
       "Content-Type": "application/json",
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
   };
